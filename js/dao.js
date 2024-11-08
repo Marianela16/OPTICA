44ed8d4c3543e39315175a2b475ba2bce261664c
chenaoh <134557141+Marianela16@users.noreply.github.com>
2024-11-08 17:53:55 -0500

    NUEVA MODIFICACION

--- js/dao.js
+++
@@ -1,6 +0,0 @@
-// DAO para gestionar productos
-async function obtenerProductos() {
-    const response = await fetch("../datos/productos.json");
-    const productos = await response.json();
-    return productos;
-}
